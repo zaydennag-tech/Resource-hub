3a5684d095a38b261df2cae827e9189fa8aa7d35
zaydennag-tech <zayden.nag@gmail.com>
2026-06-04 08:32:58 +0200

    Remove Supabase debug logs

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,7 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-console.log('SUPABASE URL:', supabaseUrl)
 
 export const supabase = createClient(
   supabaseUrl,
